8531dc8cd076732c41f91f3fc8c3ed14bfe340a7
Siva Dinesh .K <ksd.ccbp4@gmail.com>
2024-04-24 03:26:21 +0000

    coding completed

--- src/components/ReadMore/index.js
+++ src/components/ReadMore/index.js
@@ -1,1 +1,54 @@
-// Write your code here
+// importing useState hook
+import {useState} from 'react'
+
+// importing Styled components.
+import {
+  MainContainer,
+  SubCards,
+  Heading,
+  Text,
+  Image,
+  Button,
+} from './styledComponents'
+
+const ReadMore = props => {
+  const {reactHooksDescription} = props
+  const para = reactHooksDescription.slice(0, 170)
+
+  const [description, sliceDescription] = useState(para)
+  const [btnText, setBtnText] = useState('Read More')
+
+  const changeContent = () => {
+    if (btnText === 'Read More') {
+      setBtnText('Read Less')
+    } else {
+      setBtnText('Read More')
+    }
+
+    if (description.length > 170) {
+      sliceDescription(para)
+    } else {
+      sliceDescription(reactHooksDescription)
+    }
+  }
+  return (
+    <MainContainer>
+      <SubCards>
+        <Heading>React Hooks</Heading>
+        <Text>Hooks are a new addition to React</Text>
+      </SubCards>
+      <SubCards>
+        <Image
+          src="https://assets.ccbp.in/frontend/hooks/react-hooks-img.png"
+          alt="react hooks"
+        />
+        <Text para>{description}</Text>
+      </SubCards>
+      <Button type="button" onClick={changeContent}>
+        {btnText}
+      </Button>
+    </MainContainer>
+  )
+}
+
+export default ReadMore
--- src/components/ReadMore/styledComponents.js
+++ src/components/ReadMore/styledComponents.js
@@ -1,1 +1,67 @@
-// Style your elements here
+import styled from 'styled-components'
+
+// Styling container elements
+const MainContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 13px;
+`
+const SubCards = styled.div`
+  width: 80%;
+  @media (min-width: 768px) {
+    width: 600px;
+  }
+
+  display: flex;
+  flex-direction: column;
+  padding: 0 2.3vw;
+`
+
+// Styling Text content
+const Heading = styled.h2`
+  font-family: 'Roboto';
+  color: #1e293b;
+  text-align: center;
+  margin-bottom: 0;
+`
+
+const Text = styled.p`
+  font-family: 'Roboto';
+  font-size: ${props => (props.para ? '11px' : '15px')};
+  font-weight: ${props => !props.para && '500px'};
+
+  text-align: ${props => (props.para ? 'left' : 'center')};
+  line-height: 1.5;
+  color: #334155;
+`
+
+// Styling Button
+
+const Button = styled.button`
+  border: solid 1.3px #1f81ff;
+  border-radius: 4px;
+  background: #1f81ff;
+
+  padding: 4px 11px;
+  font-family: 'Roboto';
+  font-size: 14px;
+  font-weight: 600;
+
+  color: #ffffff;
+  outline: none;
+  cursor: pointer;
+`
+
+// Styling image
+const Image = styled.img`
+  width: 290px;
+  @media (min-width: 768px) {
+    width: 550px;
+  }
+  align-self: center;
+`
+export {MainContainer, SubCards, Heading, Text, Button, Image}
